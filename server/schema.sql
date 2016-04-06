9221f647d52ee3fcabc293d1993db8f11bfa61e2
nassir <nassirmz@gmail.com>
2016-04-06 01:01:57 -0700

    Establish, schema, creates tables 'messages' and 'users'

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,23 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE `messages` (
+  `id` INT UNSIGNED NOT NULL  AUTO_INCREMENT,
+  `text` VARCHAR(300),
+  `roomname` VARCHAR(100),
+  `user_id` INT(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (user_id) REFERENCES users (id)
 );
+
+CREATE TABLE `users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(100),
+  PRIMARY KEY (`id`)
+);
+
+
+
 
 /* Create other tables and define schemas for them here! */
 
