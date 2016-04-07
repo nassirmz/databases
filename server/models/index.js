c50bf6dc2a7287b1dd11b46966070e56ecd96634
nassir <nassirmz@gmail.com>
2016-04-06 23:10:12 -0700

    Finish querying on post and get

--- server/models/index.js
+++ server/models/index.js
@@ -5,14 +5,35 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
-  },
+    get: function (cb) {
+      var queryStr = "select messages.id, messages.text, messages.roomname users.username from messages \ left outer join users on (messages.user_id = users.id) \
+        order by messages.id desc";
+        db.query(queryStr, function(err, results) {
+          cb(results);
+        });
+
+    }, 
+    post: function (params, cb) {
+      var queryStr = "INSERT INTO messages(text, user_id, roomname) \
+       values (?, (SELECT id from users where username = ? limit 1), ? )";
+      db.query(queryStr, function(err, results) {
+        cb(results);
+      });
+    }
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (cb) var queryStr = "SELECT * FROM users";
+      db.query(queryStr, function(err, results) {
+        cb(results);
+      }); {
+    },
+    post: function (params, cb) {
+      var queryStr = "INSERT INTO users (username) values (?)";
+      db.query(queryStr, params, function(err, results) {
+        cb(results);
+      });
+    }
   }
 };
 
